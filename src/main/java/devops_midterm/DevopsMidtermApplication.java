114337cb16ca65f89cd83dd1e9047825bd4c411c
KIU-PC\Kiu-Student <inasaridze.sandro@kiu.edu.ge>
2025-04-20 19:37:52 +0400

    Initialize Spring Boot project

---
+++ src/main/java/devops_midterm/DevopsMidtermApplication.java
@@ -0,0 +1,13 @@
+package devops_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsMidtermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devops_midterm/DevopsMidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package devops_midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsMidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
